f585547daa50f687e0cd1f9168d78667f10bc682
Chris Cardle <cdcardle@gmail.com>
2018-02-06 17:26:37 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,2 +1,9 @@
 class CreateShows < ActiveRecord::Migration
+  def change
+    create_table :shows do |s|
+      s.string :name
+      s.string :network
+      s.datetime :day
+      s.integer :rating
+  end
 end
