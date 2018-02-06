fdcfed8eb4c6d343a1655069ae510afe0fb9ad29
Chris Cardle <cdcardle@gmail.com>
2018-02-06 17:43:04 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    self.highest_rating.name
+    self.find_by("rating = ?", self.highest_rating)
   end
 
   def self.lowest_rating
@@ -12,7 +12,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    self.lowest_rating.name
+    self.find_by("rating = ?", self.lowest_rating)
   end
 
   def self.ratings_sum
