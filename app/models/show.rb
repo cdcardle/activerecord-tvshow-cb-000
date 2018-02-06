4918bb14f04b1e87955f76742ff88476d9ef8a54
Chris Cardle <cdcardle@gmail.com>
2018-02-06 17:35:12 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,22 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    self.highest_rating.title
+    self.highest_rating.name
+  end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    self.lowest_rating.name
+  end
+
+  def self.ratings_sum
+    self.sum(:ratings)
+  end
+
+  def self.popular_shows
+    self.where(rating > 5)
   end
 end
